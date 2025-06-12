284b6c7c6de9e986d113eac636d4da3a630830c1
松永尚樹 <naoki130517ipad@gmail.com>
2025-06-12 21:29:42 +0900

    testの段階まで戻す

--- App.tsx
+++ App.tsx
@@ -4,11 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.title}>Job Rich App</Text>
-        <Text style={styles.subtitle}>SIMPLE</Text>
-        <StatusBar style="auto" />
-      </View>
+      <Text style={styles.title}>Job Rich App</Text>
+      <Text style={styles.subtitle}>SIMPLE</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -16,25 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    minHeight: '100vh',
-    width: '100%',
     backgroundColor: '#fff',
-  },
-  content: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 20,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10,
-    textAlign: 'center',
   },
   subtitle: {
     fontSize: 18,
     color: '#666',
-    textAlign: 'center',
   },
 });
